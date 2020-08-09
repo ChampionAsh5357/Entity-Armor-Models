9c7e80222d3aae5b95df533316beff0fbc185ec2
ChampionAsh5357 <championash5357@gmail.com>
2020-08-09 19:43:46 -0400

    Javadocs for ArmorModelRegistry

--- src/api/java/io/github/championash5357/entityarmormodels/client/util/ArmorModelRegistry.java
+++ src/api/java/io/github/championash5357/entityarmormodels/client/util/ArmorModelRegistry.java
@@ -9,11 +9,30 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.IArmorMaterial;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+/**
+ * Registry used to attach custom armor models to 
+ * entities. Vanilla armors are not supported since 
+ * there would be many overlaps.
+ * */
 public class ArmorModelRegistry {
 	
 	private static final Map<EntityType<?>, ArmorModel<?, ?>> MODEL_MAPPINGS = new ConcurrentHashMap<>();
 	
+	/**
+	 * Used to add custom armor models to vanilla and modded 
+	 * entities. The correct entity model needs to be used 
+	 * according to which entity it is applied to. Should be 
+	 * safe to call during {@link FMLClientSetupEvent}.
+	 * 
+	 * @param typeIn
+	 * 			The entity to apply the model to.
+	 * @param materialIn
+	 * 			The armor material of the model.
+	 * @param modelIn
+	 * 			The custom model.
+	 * */
 	public static <T extends LivingEntity, A extends EntityModel<T>> void addArmorModel(EntityType<T> typeIn, IArmorMaterial materialIn, A modelIn) {
 		getModelMappings(typeIn).addArmorModel(materialIn, modelIn);
 	}
